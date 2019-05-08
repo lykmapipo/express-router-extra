57b5ebc2bf01417ff9b8249816d781dc8570a2ce
lykmapipo <lallyelias87@gmail.com>
2019-05-09 00:33:11 +0300

    chore: update sample & examples

--- examples/simple.js
+++ examples/simple.js
@@ -4,10 +4,9 @@
 
 //dependencies
 const path = require('path');
-const express = require('express')
-// const Router = require('@lykmapipo/express-router-extra');
-const Router = require(path.join(__dirname, '..')).Router;
-const mount = require(path.join(__dirname, '..')).mount;
+const express = require('express');
+// const { Router, mountInto } = require('@lykmapipo/express-router-extra');
+const { Router, mountInto } = require(path.join(__dirname, '..'));
 const faker = require('faker');
 
 process.env.CWD = path.resolve(__dirname);
@@ -18,43 +17,43 @@ const router = new Router({ prefix: 'v', version: 1 });
 
 
 // GET /v1/users
-router.get('/users', function (request, response) {
+router.get('/users', (request, response) => {
   response.status(200).json({ data: [faker.helpers.userCard()] });
 });
 
 
 // GET /v1/users
-router.post('/users', function (request, response) {
+router.post('/users', (request, response) => {
   response.status(201).json(faker.helpers.userCard());
 });
 
 
 // GET /v1/users/:id
-router.get('/users/:id', function (request, response) {
+router.get('/users/:id', (request, response) => {
   response.status(200).json(faker.helpers.userCard());
 });
 
 
 // PUT /v1/users/:id 
-router.put('/users/:id', function (request, response) {
+router.put('/users/:id', (request, response) => {
   response.status(200).json(faker.helpers.userCard());
 });
 
 
 // PATCH /v1/users/:id 
-router.patch('/users/:id', function (request, response) {
+router.patch('/users/:id', (request, response) => {
   response.status(200).json(faker.helpers.userCard());
 });
 
 
 // DELETE /v1/users/:id 
-router.delete('/users/:id', function (request, response) {
+router.delete('/users/:id', (request, response) => {
   response.status(200).json(faker.helpers.userCard());
 });
 
 
 //mount router into app
-mount(router).into(app);
+mountInto(app, router);
 
 
 app.listen(3000);
